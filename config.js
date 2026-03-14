0ef4826cc0f8a362fec4ec255747ea2beae45ede
jkyaoen27-2706 <jk.yao.en27@gmail.com>
2026-03-14 17:22:23 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+// Configuration file for The Emerald Enigma: Royal Rescue
+// Update these values with your Google Cloud credentials
+
+window.CONFIG = window.CONFIG || {
+    // Get this from Google Cloud Console -> APIs & Services -> Credentials -> OAuth 2.0 Client ID
+    GOOGLE_CLIENT_ID: 'YOUR_CLIENT_ID_HERE.apps.googleusercontent.com',
+    
+    // Get this from Google Cloud Console -> APIs & Services -> Credentials -> API Key
+    GOOGLE_API_KEY: 'YOUR_API_KEY_HERE',
+    
+    // Get this from your Google Sheet URL: 
+    // https://docs.google.com/spreadsheets/d/SHEET_ID_HERE/edit
+    SHEET_ID: 'YOUR_SHEET_ID_HERE'
+};
